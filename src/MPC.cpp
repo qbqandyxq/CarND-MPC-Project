3e2d4a4b31f5978e7e5550aed2f51caea0e7063c
qbqandyxq <707307586@qq.com>
2018-11-30 19:44:13 +0900

    20

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -51,7 +51,7 @@ class FG_eval {
       // The part of the cost based on the reference state.
       for(int t=0;t<N; t++){
           fg[0] += CppAD::pow(vars[cte_start+t], 2);
-          fg[0] += CppAD::pow(vars[epsi_start+t], 2);
+          fg[0] += 200*CppAD::pow(vars[epsi_start+t], 2);
           fg[0] += CppAD::pow(vars[v_start+t]-ref_v,2);
       }
       //Minimisze change-rate
@@ -63,7 +63,7 @@ class FG_eval {
 //      The goal of this final loop is to make control decisions more consistent, or smoother.
 //      The next control input should be similar to the current one.
       for(int t=0;t<N-2;t++){
-          fg[0] += CppAD::pow(vars[delta_start +t + 1] - vars[delta_start+t],2);
+          fg[0] += 400*CppAD::pow(vars[delta_start +t + 1] - vars[delta_start+t],2);
           fg[0] += CppAD::pow(vars[a_start +t +1]- vars[a_start+t],2);
       }
       // Initial constraints.
